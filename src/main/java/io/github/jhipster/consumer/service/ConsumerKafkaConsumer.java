0714faf3c816ed72dd57f228d7a0275389d91a8b
Fanny Daudies <Daudies>
2019-11-15 15:42:59 +0100

    Change name of consumed kafka topic

--- src/main/java/io/github/jhipster/consumer/service/ConsumerKafkaConsumer.java
+++ src/main/java/io/github/jhipster/consumer/service/ConsumerKafkaConsumer.java
@@ -20,7 +20,7 @@ public class ConsumerKafkaConsumer {
 
     private final AtomicBoolean closed = new AtomicBoolean(false);
 
-    public static final String TOPIC = "topic_consumer";
+    public static final String TOPIC = "shared_topic";
 
     private final KafkaProperties kafkaProperties;
 
