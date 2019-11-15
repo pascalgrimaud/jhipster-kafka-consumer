c50cef66414bfb1fe220fba56796011bfcd3d050
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-11-15 10:23:16 +0100

    Initial version of consumer generated by JHipster-6.4.1 with blueprints: vuejs-1.2.0

---
+++ src/main/java/io/github/jhipster/consumer/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package io.github.jhipster.consumer.repository;
+
+import io.github.jhipster.consumer.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/io/github/jhipster/consumer/service/EmailAlreadyUsedException.java
@@ -0,0 +1,9 @@
+package io.github.jhipster.consumer.service;
+
+public class EmailAlreadyUsedException extends RuntimeException {
+
+    public EmailAlreadyUsedException() {
+        super("Email is already in use!");
+    }
+
+}
