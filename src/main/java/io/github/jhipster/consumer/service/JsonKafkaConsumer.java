cbb9f77636d43ac36708a08c6b20b6f922fe91a1
Fanny Daudies <fannydaudies@hotmail.fr>
2019-11-18 16:55:37 +0100

    Add a new consumer that consume json messages

---
+++ src/main/java/io/github/jhipster/consumer/service/JsonKafkaConsumer.java
@@ -0,0 +1,79 @@
+package io.github.jhipster.consumer.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Service
+public class JsonKafkaConsumer {
+
+    private final Logger log = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+    private final AtomicBoolean closed = new AtomicBoolean(false);
+
+    public static final String TOPIC = "json_message_topic";
+
+    private KafkaConsumer<String, JsonNode> jsonKafkaConsumer;
+
+    private String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    public JsonKafkaConsumer() { }
+
+    public void start() {
+        log.info("Kafka consumer starting...");
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+
+        this.jsonKafkaConsumer = new KafkaConsumer<>(props);
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
+
+        Thread consumerThread = new Thread(() -> {
+            try {
+                jsonKafkaConsumer.subscribe(Collections.singletonList(TOPIC));
+                log.info("Kafka consumer started");
+                while (!closed.get()) {
+                    ConsumerRecords<String, JsonNode> records = jsonKafkaConsumer.poll(Duration.ofSeconds(3));
+                    for (ConsumerRecord<String, JsonNode> record : records) {
+                        log.info("Consumed message in {} : {}", TOPIC, record.value());
+                    }
+                }
+                jsonKafkaConsumer.commitSync();
+            } catch (WakeupException e) {
+                // Ignore exception if closing
+                if (!closed.get()) throw e;
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            } finally {
+                jsonKafkaConsumer.close();
+            }
+        });
+        consumerThread.start();
+    }
+
+    public KafkaConsumer<String, JsonNode> getJsonKafkaConsumer() {
+        return jsonKafkaConsumer;
+    }
+
+    public void shutdown() {
+        log.info("Shutdown Kafka json consumer");
+        closed.set(true);
+        jsonKafkaConsumer.wakeup();
+    }
+}
